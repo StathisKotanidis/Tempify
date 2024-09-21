86db2d6e0f8e1e262c24299b2497cc8e6af148d7
StathisKotanidis <stathiskotanidhs@gmail.com>
2024-09-21 15:50:42 +0300

    fetched the api based on the user input and clicking onthe search icon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
+import { useEffect } from "react";
 import "./App.css";
+import { useState } from "react";
+
+const apiKey = "20ff8e16ee4a79b95bc295e19a5bf2d1";
 
 export default function App() {
+  const [cityName, setCityName] = useState("");
+  const [enableApi, setEnableApi] = useState(false);
+
+  function handleCityName(e) {
+    setCityName(e.target.value);
+  }
+
+  function handleEnableApi() {
+    setEnableApi(true);
+  }
+
+  useEffect(() => {
+    if (!enableApi) return; // only fetch if enable api is true
+    async function getWeather() {
+      try {
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`
+        );
+
+        if (!res.ok)
+          throw new Error("Something went wrong with fetching the weather");
+
+        const data = await res.json();
+        console.log(data);
+        console.log(data.main.temp); // temperature , Math.ceil()
+        console.log(data.wind.speed); // wind speed
+        console.log(data.main.humidity); // humidity percentage
+        console.log(data.weather[0].icon); // icon of corresponding weather result
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getWeather();
+    setEnableApi(false);
+  }, [enableApi, cityName]); // Dependency array to re-trigger on enableApi or cityName change
+
   return (
     <div
       className="app-container"
       style={{ backgroundImage: `url(${"/assets/fog.gif"})` }}
     >
       <div className="searchbar-container">
-        <SearchBar />
+        <SearchBar onCityName={handleCityName} onEnableApi={handleEnableApi} />
         <TemperatureConverter />
       </div>
       <LocalTime />
@@ -20,11 +60,15 @@ export default function App() {
   );
 }
 
-function SearchBar() {
+function SearchBar({ onCityName, onEnableApi }) {
   return (
     <div className="searchbar-wrapper">
-      <input className="searchbar" placeholder="Search for a city.."></input>
-      <i className="bx bx-search searchbar-btn"></i>
+      <input
+        onChange={onCityName}
+        className="searchbar"
+        placeholder="Search for a city.."
+      ></input>
+      <i onClick={onEnableApi} className="bx bx-search searchbar-btn"></i>
     </div>
   );
 }
@@ -53,7 +97,7 @@ function CurrentTemperature() {
       <img src="./rain.png" alt=" weather-image"></img>
       <span className="current-temperature">25°C</span>
       <span className="city-wrapper">
-        <i class="bx bx-current-location"></i>
+        <i className="bx bx-current-location"></i>
         <span className="city">Miami</span>
       </span>
     </div>
